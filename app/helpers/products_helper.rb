9107b92c5c1bce170fe264c88c7ee5163e6aaf10
Adrian Cuadros <adrian@innku.com>
2009-11-16 23:39:16 -0600

    Administracion adaptada a medias

--- app/helpers/products_helper.rb
+++ app/helpers/products_helper.rb
@@ -2,7 +2,7 @@ module ProductsHelper
   def categories_for_select
    options = "<option value='-1' selected='selected'></option>"
    options +=  options_from_collection_for_select(Category.all, 'id', 'name')
-   options += "<option value='0'>Nueva categor&iacute;a...</option>"
+   options += "<option value='0'>New Category...</option>"
    options
   end
   
