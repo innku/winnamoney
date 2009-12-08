e78df2de2ed3d4f6e04e418cfcae88ba911cc0a0
Adrian Cuadros <adrian@innku.com>
2009-12-08 02:42:39 -0600

    Se agrega la opcion de submodulos

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,8 +1,8 @@
 #------------  Deploy with Git ----------------------
 
 set :application, "winnamoney.innku.com"
-set :rails_env, "staging"
-set :base_path, "staging"
+set :rails_env, "production"
+set :base_path, "production"
 set :use_sudo,  false
 
 default_run_options[:pty] = true
@@ -12,7 +12,7 @@ set :scm_passphrase, "soyinnku09"
 set :user, "winna"
 
 set :deploy_to, "/home/#{user}/#{base_path}"
-set :branch, "staging"
+set :branch, "master"
 set :deploy_via,  :remote_cache
 set :git_enable_submodules, true
 
